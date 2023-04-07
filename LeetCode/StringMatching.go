6987ce6115722381c25b0a68fbbf0b02cdd422da
jsethi7133 <jsethi7133@gmail.com>
2023-04-07 18:01:56 +0530

    Create StringMatching.go

---
+++ LeetCode/StringMatching.go
@@ -0,0 +1,15 @@
+func strStr(haystack string, needle string) int {
+    m:=len(needle)
+    n:=len(haystack)
+    for i:=0;i<n-m;i++{
+        for j:=0;j<m;j++{
+            if needle[j]!=haystack[i+j]{
+                break
+            }
+            if j==m-1{
+                return i
+            }
+        }
+    }
+    return -1
+}
