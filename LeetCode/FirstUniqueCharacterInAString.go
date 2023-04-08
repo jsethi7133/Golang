b928efa0de344e86eeffddc7ff8e74faec7dd91e
jsethi7133 <jsethi7133@gmail.com>
2023-04-08 22:45:46 +0530

    Create FirstUniqueCharacterInAString.go

---
+++ LeetCode/FirstUniqueCharacterInAString.go
@@ -0,0 +1,12 @@
+func firstUniqChar(s string) int {
+  res:=make([]int, 26)
+  for i:=0;i<len(s);i++{
+    res[s[i]-'a']++
+  }
+  for i:=0;i<len(s);i++{
+    if res[s[i]-'a']==1{
+      return i
+    }
+  }
+  return -1
+}
