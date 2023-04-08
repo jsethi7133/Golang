ff5e74984dd4df961774ba9f1488d31dde40fe62
jsethi7133 <jsethi7133@gmail.com>
2023-04-08 19:26:10 +0530

    Update MoveZeroes.go

--- LeetCode/MoveZeroes.go
+++ LeetCode/MoveZeroes.go
@@ -5,11 +5,11 @@ func moveZeroes(nums []int)  {
             res=append(res,nums[i])
         }
     }
-    fmt.Println(res)
+
     for j:=0;j<len(nums)-len(res)+1;j++{
         res=append(res,0)
     }
-    fmt.Println(res)
+
     for k:=0;k<len(nums);k++{
         nums[k]=res[k]
     }
